d7854ef64f5934feed97921dbd6310fe7323a93c
Eli Meister <etmeister@gmail.com>
2018-06-18 22:57:00 -0400

    Syncing sound clips with start of second

--- serialmonitor.py
+++ serialmonitor.py
@@ -1,6 +1,8 @@
 import serial
 import json 
 import subprocess
+import time
+
 states = { 15: False, 16: False, 17: False, 18: False, 19: False, 22: False } 
 
 files = { 15: "Vibes 1.m4a", 16: "Wood Blocks.m4a", 17: "Marimba.m4a", 18: "Taiko Drums.m4a", 19: "Glockenspiel 1.m4a", 22: "Tympani.m4a" }
@@ -8,12 +10,11 @@ files = { 15: "Vibes 1.m4a", 16: "Wood Blocks.m4a", 17: "Marimba.m4a", 18: "Taik
 procs = { 15: False, 16: False, 17: False, 18: False, 19: False, 22: False }
 
 def playSound(button):
-    print("Playing " + files[button])
     procs[button] = subprocess.Popen(["afplay","/Users/eimeister/Documents/Music/"+ files[button] ])
 
 
 
-with serial.Serial('/dev/cu.usbmodem4012401', 9600, timeout=1) as ser:
+with serial.Serial('/dev/cu.usbmodem4012401', 38400, timeout=1) as ser:
     while True:
         line = ser.readline().rstrip()
         try: 
@@ -21,11 +22,13 @@ with serial.Serial('/dev/cu.usbmodem4012401', 9600, timeout=1) as ser:
         except ValueError:
             print("Error reading button status")
             continue
+        loopStart = str(time.time()).split('.')[1][0]
         for key in states:
             if key in buttons:
                 if states[key] is not True:
-                     states[key] = True
-                     playSound(key)
+                     if loopStart == "0":
+                         states[key] = True
+                         playSound(key)
                 else:
                     procs[key].poll()
                     if procs[key].returncode is not None:
